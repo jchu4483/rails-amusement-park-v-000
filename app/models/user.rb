a9ffb81faf7b04798b1c83e983150fdb84458d15
jchu4483 <jchu4483@gmail.com>
2016-09-10 18:28:17 -0400

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   has_many :attractions, through: :rides
 
   has_secure_password
+
+  def mood
+    if happiness && nausea
+      happiness > nausea ? 'happy' : 'sad'
+    end
+  end
 end
